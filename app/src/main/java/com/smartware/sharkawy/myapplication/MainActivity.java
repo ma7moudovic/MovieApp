bf0733876a9f792f4b4c4e7491e3c1e6b985ed27
ma7moudovic <ma7moudovic@hotmail.com>
2016-01-27 17:08:40 +0200

    dummey comment added testing

--- app/src/main/java/com/smartware/sharkawy/myapplication/MainActivity.java
+++ app/src/main/java/com/smartware/sharkawy/myapplication/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hey its an update
-        Toast.makeText(this,"bazinga",Toast.LENGTH_SHORT).show();
     }
 }
